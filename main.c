b6730fbd1c8d17af7b1ab988a040ed18f9278542
SuianiAlmeida <developer.suiani@gmail.com>
2025-01-19 13:04:17 -0300

    editando função while

--- main.c
+++ main.c
@@ -21,7 +21,6 @@ int main()
 
     stdio_init_all();
 
-    // Inicialização dos pinos (essa parte deve ser feita por um responsável por outra funcionalidade, se necessário)
     gpio_init(GREEN_LED_PIN);
     gpio_set_dir(GREEN_LED_PIN, GPIO_OUT);
     gpio_init(BLUE_LED_PIN);
@@ -31,3 +30,15 @@ int main()
 
     while (true) {
         scanf("%s", word);
+
+        if (strcmp(word, "green") == 0 || strcmp(word, "verde") == 0) {
+            // Ligar o LED verde e desligar os outros
+            printf("ON: GREEN\n");
+        } else if (strcmp(word, "blue") == 0 || strcmp(word, "azul") == 0) {
+            // Ligar o LED azul e desligar os outros
+            printf("ON: BLUE\n");
+        } else if (strcmp(word, "red") == 0 || strcmp(word, "vermelho") == 0) {
+            // Ligar o LED vermelho e desligar os outros
+        } else if (strcmp(word, "white") == 0 || strcmp(word, "branco") == 0) {
+            // Ligar todos os LEDs
+            printf("ON: WHITE\n");
