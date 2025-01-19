2755c501d036eeb30a8a07f48a8f795fa12e143f
SuianiAlmeida <developer.suiani@gmail.com>
2025-01-19 12:36:54 -0300

    adicionando o turn_off_leds

--- main.c
+++ main.c
@@ -14,21 +14,3 @@ void turn_off_leds() {
     gpio_put(BLUE_LED_PIN, 0);  // Desliga o LED azul
     gpio_put(RED_LED_PIN, 0);   // Desliga o LED vermelho
 }
-
-int main()
-{
-    char word[32]; // Define um buffer para armazenar a palavra digitada
-
-    stdio_init_all();
-
-    gpio_init(GREEN_LED_PIN); 
-    gpio_set_dir(GREEN_LED_PIN, GPIO_OUT);
-    gpio_init(BLUE_LED_PIN); 
-    gpio_set_dir(BLUE_LED_PIN, GPIO_OUT);
-    gpio_init(RED_LED_PIN); 
-    gpio_set_dir(RED_LED_PIN, GPIO_OUT);
-
-    gpio_init(BUZZER_PIN);
-    gpio_set_dir(BUZZER_PIN, GPIO_OUT);
-
- 
